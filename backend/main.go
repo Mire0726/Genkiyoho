4ec1f266e69156345191d3494662e152de68328d
sumireinoue <violeta.jin.726@gmail.com>
2024-02-11 15:22:38 +0900

    環境構築

---
+++ backend/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"context"
+    "log"
+	"Genkiyoho/ent"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	//Ginフレームワークのデフォルトの設定を使用してルータを作成
+	router := gin.Default()
+	//PostgreSQLに接続
+    client, err := ent.Open("postgres", "host=db port=5432 user=postgres dbname=db password=password sslmode=disable")
+
+	if err != nil {
+		log.Fatalf("failed opening connection to postgres: %v", err)
+	}
+	// Run the auto migration tool.
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+	
+	// ルートハンドラの定義
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello, World!",
+		})
+	})
+
+	// サーバー起動
+	router.Run(":8080")
+}
