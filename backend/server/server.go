f1d5d212124842f03d7011d0f7fd57781752daea
sumireinoue <violeta.jin.726@gmail.com>
2024-02-16 16:18:06 +0900

    sql設定

--- backend/server/server.go
+++ backend/server/server.go
@@ -3,10 +3,43 @@ package server
 import (
 	"github.com/Mire0726/Genkiyoho/backend/handler"
 	"net/http"
+	"github.com/labstack/echo/v4"
+	"log"
+	echomiddleware "github.com/labstack/echo/v4/middleware"
 )
 
-func StartServer() {
-    http.HandleFunc("/users", handler.GetUser)
-    http.ListenAndServe(":8080", nil)
+// Serve はHTTPサーバを起動します
+func Serve(addr string) {
+	e := echo.New()
+
+	// ミドルウェアの設定
+	e.Use(echomiddleware.Recover())
+	e.Use(echomiddleware.CORS())
+
+	// ルーティングの設定
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Welcome to GEnkiyoho!")
+	})
+
+	//APIのルート
+	http.HandleFunc("/users", handler.GetUser)
+
+	// // POSTリクエストのルート
+    // e.POST("/todo", handler.HandleTodoCreate)
+	// e.GET("/todos", handler.HandleGetAllTodos)
+	// e.PUT("/todo", handler.HandleUpdateTodoByTitle)
+	// e.DELETE("/todo/:id", handler.HandleDeleteTodo)
+
+
+	// サーバーの起動
+	log.Printf("Server running on %s", addr)
+	if err := e.Start(addr); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
+
+// func StartServer() {
+//     http.HandleFunc("/users", handler.GetUser)
+//     http.ListenAndServe(":8080", nil)
+// }
 
