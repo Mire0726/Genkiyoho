f109fdb9db6865fe9d1d82af59028097744465b8
sumireinoue <violeta.jin.726@gmail.com>
2024-03-17 18:53:36 +0900

    最初のルーティング

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const HomePage = () => {
-  return <div>go to app
-  </div>;
+  const router = useRouter();
+
+  useEffect(() => {
+    // コンポーネントのマウント時に/loginにリダイレクト
+    router.push('/login');
+  }, [router]); // 依存配列にrouterを指定
+
+  return <div>Redirecting to login...</div>;
 };
 
 export default HomePage;
