4c97abae09f568a2524fd2b03ee3c1123317408f
sumireinoue <violeta.jin.726@gmail.com>
2024-04-05 19:14:44 +0900

    PORTセット

--- backend/main.go
+++ backend/main.go
@@ -4,18 +4,34 @@ import (
 	"flag"
 
 	"github.com/Mire0726/Genkiyoho/backend/server"
-
+    "os"
+    "fmt"
+    "log"
 	
 )
 
+// func main() {
+//     var addr string
+//     flag.StringVar(&addr, "addr", "8080", "Address to listen on")
+//     flag.Parse()
+
+//     // サーバーの起動
+//     server.Serve(addr)
+
+// }
+
 func main() {
-    var addr string
-    flag.StringVar(&addr, "addr", ":8080", "Address to listen on")
-    flag.Parse()
+	// 環境変数PORTからポート番号を取得。指定されていない場合はデフォルトで"8080"を使用。
+	var defaultPort = "8080"
+	var port = os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+		flag.StringVar(&port, "addr", defaultPort, "default server port")
+	}
+	flag.Parse()
 
-    // サーバーの起動
-    server.Serve(addr)
-
+	// サーバーの設定と起動
+	addr := fmt.Sprintf(":%s", port)
+	log.Printf("Listening on %s...\n", addr)
+	server.Serve(addr)
 }
-
-
