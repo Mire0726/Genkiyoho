8f95b610eea4ca048e71d4ddae553f87c634cee9
sumireinoue <violeta.jin.726@gmail.com>
2024-04-05 16:47:03 +0900

    jawsdb設定

--- backend/server/db/db.go
+++ backend/server/db/db.go
@@ -2,7 +2,7 @@ package db
 
 import (
 	"database/sql"
-	"fmt"
+	// "fmt"
 	"log"
 	"os"
 
@@ -22,22 +22,31 @@ func init() {
 	// 	log.Fatal("Error loading .env file")
 	// }
 
-	user := os.Getenv("MYSQL_USER")
-	password := os.Getenv("MYSQL_PASSWORD")
-	host := os.Getenv("MYSQL_HOST")
-	port := os.Getenv("MYSQL_PORT")
-	database := os.Getenv("MYSQL_DATABASE")
-	charset := os.Getenv("MYSQL_CHARSET")
-	parseTime := os.Getenv("MYSQL_PARSE_TIME")
-	loc := os.Getenv("MYSQL_LOC")
+	// user := os.Getenv("MYSQL_USER")
+	// password := os.Getenv("MYSQL_PASSWORD")
+	// host := os.Getenv("MYSQL_HOST")
+	// port := os.Getenv("MYSQL_PORT")
+	// database := os.Getenv("MYSQL_DATABASE")
+	// charset := os.Getenv("MYSQL_CHARSET")
+	// parseTime := os.Getenv("MYSQL_PARSE_TIME")
+	// loc := os.Getenv("MYSQL_LOC")
 	
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=%s&loc=%s",
-		user, password, host, port, database, charset, parseTime, loc)
+	// dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=%s&loc=%s",
+	// 	user, password, host, port, database, charset, parseTime, loc)
+	
+	jawsdbURL := os.Getenv("JAWSDB_URL")
+    if jawsdbURL == "" {
+        log.Fatal("JAWSDB_URL environment variable is not set")
+    }
 
-	Conn, err := sql.Open(driverName, dsn)
-	if err != nil {
-		log.Fatal(err)
-	}
+    Conn, err := sql.Open(driverName, jawsdbURL)
+    if err != nil {
+        log.Fatal(err)
+    }
+	// Conn, err := sql.Open(driverName, dsn)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 	if err := Conn.Ping(); err != nil {
 		log.Fatal("Unable to connect to the database:", err)
 	}
